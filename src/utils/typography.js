72e129cedbb8ba8271e4d9e2e597a59c0b6be0ea
Nate <nshirley22@gmail.com>
2020-05-02 14:05:04 -0400

    msg

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
+import StAnnes from "typography-theme-st-annes"
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
@@ -11,7 +12,7 @@ Wordpress2016.overrideThemeStyles = () => {
 
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(StAnnes)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
